5878656680eb64d49943f1f70e5be4a427f5235e
Arturacu <arturacu@ucm.es>
2018-04-16 16:32:58 +0200

    Updated
    Switching lights working on!

--- src/main/java/pr5/model/MostCrowdedJunction.java
+++ src/main/java/pr5/model/MostCrowdedJunction.java
@@ -3,11 +3,28 @@ package pr5.model;
 /**Creates a new most crowded junction.
  * @author Inmapg
  * @author Arturacu
- * @version 3.0
+ * @version 2.0
  */
 public class MostCrowdedJunction extends TimeSliceJunction{
     
-    public MostCrowdedJunction(String id){
+    public MostCrowdedJunction(String id) {
         super(id);
+        
+    }
+ @Override
+    protected void switchLights() {
+        if (nextRoad == null || !nextRoad.hasNext()) {
+            nextRoad = incomingRoadMap.keySet().iterator();
+        }
+        if(currentRoad == null){
+            currentRoad = nextRoad.next();
+            incomingRoadMap.get(currentRoad).onGreenLight();
+            incomingRoadMap.get(currentRoad).advanceFirstVehicle();
+           // lastGreenLightRoad = currentRoad; not needed here, the switching lights control has been modified in this type of junction
+        }
+        else{
+            
+        }
+        
     }
 }
